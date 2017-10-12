ce05bd8fb5e43ae8b51a438b6d5200fabf48b3a0
Eitan Isaacson <eitan@monotonous.org>
2017-10-12 12:02:32 -0700

    linted js files

--- background.js
+++ background.js
@@ -1,31 +1,29 @@
-//In background.js:
+// In background.js:
 // React when a browser action's icon is clicked.
 
-var settings;
-var intervalId;
+/* exported updateSettings */
 
 function updateAllTabs() {
-    chrome.tabs.query({}, function(tabs) { 
-	    for (var index = 0; index < tabs.length; index++ ) {
-	    	var tab = tabs[index];
-	    	chrome.tabs.sendMessage(tab.id, { type: 'refresh', settings: window.settings }, function(response) {});
-	   }
-    });
+  chrome.tabs.query({}, function(tabs) {
+    for (var index = 0; index < tabs.length; index++) {
+      var tab = tabs[index];
+      chrome.tabs.sendMessage(tab.id, { type: 'refresh', settings: window.settings }, function() {});
+    }
+  });
 }
 
 function updateSettings(settings) {
-	window.settings = settings;
-	updateAllTabs();
+  window.settings = settings;
+  updateAllTabs();
 
-	if (!window.intervalId) {
-        window.intervalId = setInterval(updateAllTabs, 500); // In case zoom changes
-    }
+  if (!window.intervalId) {
+    window.intervalId = setInterval(updateAllTabs, 500); // In case zoom changes
+  }
 }
 
-
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.type === "getSettings" && window.settings) {
-    	chrome.tabs.sendMessage(sender.tab.id, { type: "refresh", settings: window.settings }, function(response) {});
+  function(request, sender) {
+    if (request.type === 'getSettings' && window.settings) {
+      chrome.tabs.sendMessage(sender.tab.id, { type: 'refresh', settings: window.settings }, function() {});
     }
-});
+  });
