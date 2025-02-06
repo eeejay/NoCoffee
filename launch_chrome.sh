69004a4dee742adf7e424e181e8159934f6d608b
Corina Murg <corina.murg@gmail.com>
2025-02-06 13:57:16 -0500

    update launch for windows

--- launch_chrome.sh
+++ launch_chrome.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+# #!/bin/sh
 
-DATA_DIR=`mktemp -d` &&
-echo 'Temporary dir:' $DATA_DIR &&
-touch $DATA_DIR/First\ Run &&
-google-chrome --user-data-dir=$DATA_DIR --password-store=basic --load-extension=. $*
-rm -rf $DATA_DIR
+# DATA_DIR=`mktemp -d` &&
+# echo 'Temporary dir:' $DATA_DIR &&
+# touch $DATA_DIR/First\ Run &&
+# google-chrome --user-data-dir=$DATA_DIR --password-store=basic --load-extension=. $*
+# rm -rf $DATA_DIR
+
+
+#!/bin/bash
+DATA_DIR=$(mktemp -d)
+echo "Temporary dir: $DATA_DIR"
+touch "$DATA_DIR/First Run"
+"/c/Program Files/Google/Chrome/Application/chrome.exe" --user-data-dir="$DATA_DIR" --password-store=basic --load-extension="$(pwd)" "$@"
+rm -rf "$DATA_DIR"
