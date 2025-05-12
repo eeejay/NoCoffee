5d8e637d08e2a836f817e680a2d916aaf20e9888
Corina Murg <corina.murg@gmail.com>
2025-05-12 13:32:15 -0400

    change brightness value for glare filter

--- contentscript.js
+++ contentscript.js
@@ -259,6 +259,7 @@ function updateCursorEffects(view, viewData) {
   let cursorFilters = [];
   if (view.doc.cssFilter) cursorFilters.push(view.doc.cssFilter);
  
+  // not needed since the filters are already applied to the body
   // if (viewData.blur) {
   //   cursorFilters.push(`blur(${viewData.blur}px)`);
   // }
@@ -716,7 +717,8 @@ function getViewData(settings) {
     blur: (blurPlusExtra / zoom) / 2,
     contrast: settings.contrastLevel ? 60 - settings.contrastLevel : 100,
     colorMatrixValues: settings.colorDeficiencyMatrixValues,
-    brightness: settings.brightnessLevel * 1.2,
+    // brightness: settings.brightnessLevel * 1.2,
+    brightness: 100 + (settings.brightnessLevel * 2),
     ghosting: settings.ghostingLevel / zoom,
     snow: { zoom: zoom, amount: 0.03 * settings.snowLevel },
     cloudy: { zoom: zoom, cloudyLevel: settings.cloudyLevel * 6 },
